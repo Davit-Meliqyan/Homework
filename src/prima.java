c10d95a0226c48e543aa366ec4809556d57fc2f4
Davit-Meliqyan <davidmeliqyan6@gmail.com>
2022-02-07 12:49:27 +0400

    add Homework 3

--- src/prima.java
+++ src/prima.java
@@ -3,20 +3,24 @@ public class prima {
     public static void main(String[] args) {
         //4
 
-        int count = 0;
-        int n = 2;
-        int sum = 0;
-        while (count < 6) {
-            if (isPrime(n)) {
-                count++;
-                sum = sum + n;
-            }
-            n++;
-        }
-        System.out.println(isPrime(4));
-        System.out.println(sum);
+//        int count = 0;
+//        int n = 2;
+//        int sum = 0;
+//        while (count < 6) {
+//            if (isPrime(n)) {
+//                count++;
+//                sum = sum + n;
+//            }
+//            n++;
+//        }
+//        System.out.println(isPrime(4));
+//        System.out.println(sum);
 
-        //  \u000a System.out.println("dd");
+        int i =-15;
+
+        //  \u000a i = 10;
+
+        System.out.println(i);
     }
 
 
