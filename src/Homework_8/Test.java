1350c9d28d0529ba65b3856052494db6efd4404d
Davit-Meliqyan <davidmeliqyan6@gmail.com>
2022-02-11 12:50:58 +0400

    add Homework 8

--- src/Homework_8/Test.java
+++ src/Homework_8/Test.java
@@ -47,11 +47,17 @@ public class Test {
         String abs = "";
         System.out.println("input number :");
         double n = scanner.nextDouble();
-        if (n > 0) sign = "Positive";
-        else if (n < 0) sign = "Negative";
+        if (n > 0) {
+            sign = "Positive";
+        } else if (n < 0) {
+            sign = "Negative";
+        }
 
-        if (Math.abs(n) > 1000000) abs = "large";
-        else if (Math.abs(n) < 1 && Math.abs(n) != 0) abs = "small";
+        if (Math.abs(n) > 1000000) {
+            abs = "large";
+        } else if (Math.abs(n) < 1 && Math.abs(n) != 0) {
+            abs = "small";
+        }
 
 
         System.out.println(sign + " " + abs);
@@ -60,8 +66,9 @@ public class Test {
     public static void iceCream() {
         System.out.print("input number :");
         int n = scanner.nextInt();
-        if (n == 0 || n == 1 || n == 2 || n == 4 || n == 7) System.out.print("No");
-        else {
+        if (n == 0 || n == 1 || n == 2 || n == 4 || n == 7) {
+            System.out.print("No");
+        } else {
             System.out.println("Yes");
             int count = 0;
             int res = n;
@@ -78,22 +85,42 @@ public class Test {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
-        if (c >= a && c >= b) System.out.println("greatest number " + c);
-        else if (b >= a && b >= c) System.out.println("greatest number " + b);
-        else System.out.println("greatest number " + a);
+        if (c >= a && c >= b) {
+            System.out.println("greatest number " + c);
+        } else if (b >= a && b >= c) {
+            System.out.println("greatest number " + b);
+        } else {
+            System.out.println("greatest number " + a);
+        }
     }
 
     public static void weekday() {
         int n = scanner.nextInt();
         if (n > 0 && n < 8) {
-            if (n == 1) System.out.println("Sunday");
-            if (n == 2) System.out.println("Monday");
-            if (n == 3) System.out.println("Tuesday");
-            if (n == 4) System.out.println("Wednesday");
-            if (n == 5) System.out.println("Thursday");
-            if (n == 6) System.out.println("Friday");
-            if (n == 7) System.out.println("Saturday");
-        } else System.err.println("ERROR");
+            if (n == 1) {
+                System.out.println("Sunday");
+            }
+            if (n == 2) {
+                System.out.println("Monday");
+            }
+            if (n == 3) {
+                System.out.println("Tuesday");
+            }
+            if (n == 4) {
+                System.out.println("Wednesday");
+            }
+            if (n == 5) {
+                System.out.println("Thursday");
+            }
+            if (n == 6) {
+                System.out.println("Friday");
+            }
+            if (n == 7) {
+                System.out.println("Saturday");
+            }
+        } else {
+            System.err.println("ERROR");
+        }
 
     }
 
@@ -115,8 +142,11 @@ public class Test {
         int high = 10000;
         int result = (int) (Math.random() * ((high - low)) + low + 1);
         System.out.println(result);
-        if ((result > -10000 && result < 10000) && (result % 3 == 0 || result % 5 == 0)) System.out.println(true);
-        else System.out.println(false);
+        if ((result > -10000 && result < 10000) && (result % 3 == 0 || result % 5 == 0)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 
     public static void multipleOf7() {
@@ -127,27 +157,47 @@ public class Test {
         if ((a > 0 && b > 0) && (a % 7 == 0 || b % 7 == 0)) {
             if (a > b) {
                 if (b % 2 == 0) {
-                    for (int i = b + 1; i <= a; i += 2) System.out.print(i + " ");
-                } else for (int i = b; i <= a; i += 2) System.out.print(i + " ");
+                    for (int i = b + 1; i <= a; i += 2) {
+                        System.out.print(i + " ");
+                    }
+                } else for (int i = b; i <= a; i += 2) {
+                    System.out.print(i + " ");
+                }
             } else {
                 if (a % 2 == 0) {
-                    for (int i = a; i <= b; i += 2) System.out.print(i + " ");
-                } else for (int i = a + 1; i <= b; i += 2) System.out.print(i + " ");
+                    for (int i = a; i <= b; i += 2) {
+                        System.out.print(i + " ");
+                    }
+                } else for (int i = a + 1; i <= b; i += 2) {
+                    System.out.print(i + " ");
+                }
             }
         }
     }
 
-    public static void quarterOfPoint() {
-        System.out.println("Input x coordinates");
-        int x = scanner.nextInt();
-        System.out.println("Input y coordinates");
-        int y = scanner.nextInt();
-        if (x == 0 || y == 0) System.out.println("Not in the same quarter");
-        if (x > 0 && y > 0) System.out.println("1 quarter");
-        if (x < 0 && y > 0) System.out.println("2 quarter");
-        if (x < 0 && y < 0) System.out.println("3 quarter");
-        if (x > 0 && y < 0) System.out.println("4 quarter");
+    public static boolean quarterOfPoint(int x1 ,int y1, int x2 , int y2) {
 
+        if (x1 == 0 || y1 == 0 || x2 == 0 || y2 == 0) {
+            System.out.println("Not in the same quarter");
+            return true;
+        }
+        else if (x1 > 0 && y1 > 0 && x2 > 0 && y2 > 0) {
+            System.out.println("1 quarter");
+            return true;
+        }
+        else if (x1 < 0 && y1 > 0 && x2 < 0 && y2 > 0) {
+            System.out.println("2 quarter");
+            return true;
+        }
+        else if (x1 < 0 && y1 < 0 && x2 < 0 && y2 < 0) {
+            System.out.println("3 quarter");
+            return true;
+        }
+        else if (x1 > 0 && y1 < 0 && x2 > 0 && y2 < 0) {
+            System.out.println("4 quarter");
+            return true;
+        }
+        else return false;
     }
 
     public static void degenerateTriangle() {
@@ -160,8 +210,11 @@ public class Test {
         if (a == 0 || b == 0 || c == 0) {
             System.out.println("triangle is degenerate");
         } else {
-            if (a + b > c && a + c > b && b + c > a) System.out.println("triangle is not degenerate");
-            else System.out.println("triangle is degenerate");
+            if (a + b > c && a + c > b && b + c > a) {
+                System.out.println("triangle is not degenerate");
+            } else {
+                System.out.println("triangle is degenerate");
+            }
         }
 
 
@@ -174,10 +227,13 @@ public class Test {
         int num2 = scanner.nextInt();
         System.out.println("Input number 3");
         int num3 = scanner.nextInt();
-        if ((num1 == num2 && num1 != num3) || (num1 == num3 && num1 != num2) || (num2 == num3 && num1 != num3))
+        if ((num1 == num2 && num1 != num3) || (num1 == num3 && num1 != num2) || (num2 == num3 && num1 != num3)) {
             System.out.println(2);
-        else if (num1 == num2 && num2 == num3) System.out.println(3);
-        else System.out.println("Numbers is not equals ");
+        } else if (num1 == num2 && num2 == num3) {
+            System.out.println(3);
+        } else {
+            System.out.println("Numbers is not equals ");
+        }
     }
 
     public static void quadraticEquation() {
@@ -187,11 +243,13 @@ public class Test {
         double b = scanner.nextDouble();
         System.out.println("Input c ");
         double c = scanner.nextDouble();
-        if (a == 0) System.out.println("Not quadratic equation");
-        else {
+        if (a == 0) {
+            System.out.println("Not quadratic equation");
+        } else {
             double D = b * b - 4 * a * c;
-            if (D < 0) System.out.println("quadratic equation has no real solution");
-            else if (D == 0) {
+            if (D < 0) {
+                System.out.println("quadratic equation has no real solution");
+            } else if (D == 0) {
                 double x = -b / 2 * a;
                 System.out.println("quadratic equation has 1 real solution x = " + x);
             } else {
@@ -224,41 +282,53 @@ public class Test {
             } else {
                 if (a > c) {
                     System.out.println(c + "\n" + a + "\n" + b);
-                } else System.out.println(a + "\n" + c + "\n" + b);
+                } else {
+                    System.out.println(a + "\n" + c + "\n" + b);
+                }
             }
-
         } else {
             if (a <= c) {
                 System.out.println(b + "\n" + a + "\n" + c);
-            }
-            else {
-                if(b > c){
+            } else {
+                if (b > c) {
                     System.out.println(c + "\n" + b + "\n" + a);
+                } else {
+                    System.out.println(b + "\n" + c + "\n" + a);
                 }
-                else System.out.println(b + "\n" + c + "\n" + a);
             }
         }
-
-
     }
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 //        eligibleToVote();
 //        isEven();
 //        printRandom();
 //        multiple5();
 //        greatestNumber();
 //        iceCream();
-  //     printAbs();
+//        printAbs();
 //        weekday();
 //        leapYear();
 //        randomFilter();
 //        multipleOf7();
-//        quarterOfPoint();
+
+        System.out.println("Input x1");
+        int x1 = scanner.nextInt();
+        System.out.println("Input y1");
+        int y1 = scanner.nextInt();
+        System.out.println("Input x2");
+        int x2 = scanner.nextInt();
+        System.out.println("Input y2");
+        int y2 = scanner.nextInt();
+
+
+        boolean bool = quarterOfPoint(x1,y1,x2,y2);
+        System.out.println(bool);
 //        degenerateTriangle();
 //        equalsNumbers();
 //        quadraticEquation();
-       // nonDecreasingOrder();
+        // nonDecreasingOrder();
     }
 }
